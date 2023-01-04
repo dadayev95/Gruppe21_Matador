e61f274ffe549c4e9f30ed8f40f57b1377483c14
dadayev95 <dadayev95@gmail.com>
2023-01-04 11:56:05 +0100

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
-package org.example;public class Main {
+import gui_main.GUI;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GUI gui = new GUI();
     }
 }
